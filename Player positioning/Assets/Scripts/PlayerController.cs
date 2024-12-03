80307368d40062152aee513fecfe509088f13a28
Jazleen <kaurj3@roverkids.org>
2024-12-03 13:50:11 -0500

    2.2 completed

--- Player positioning/Assets/Scripts/PlayerController.cs
+++ Player positioning/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+
+    public GameObject projectilePrefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,6 +29,11 @@ public class NewMonoBehaviourScript : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput *  Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
 
     }
 }
